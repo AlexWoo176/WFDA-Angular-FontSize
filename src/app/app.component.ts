708bc8d24fe47dbeebeba573504db125b6dfe866
AlexWoo176 <phuong.tuan2012@gmail.com>
2020-07-02 17:17:39 +0700

    Complete FontSize

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'WFDA-Angular-FontSize';
+  title = 'font-size-editor';
 }
